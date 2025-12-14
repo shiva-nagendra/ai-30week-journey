d6cd8cc422d999c7bbc8f9db45363b1dfc5dbb5a
Shiva Nagendra <shivanagendra01@gmail.com>
2025-12-14 16:36:34 +0530

    Day 19: Recursion mastery, call stack tracing, and recursive patterns

---
+++ day_19.py
@@ -0,0 +1,27 @@
+#Recursion Mastery
+
+def countdown(n):
+    if n == 0:          # base case
+        print("Done")
+        return
+    print(n)
+    countdown(n-1) # recursive case
+
+countdown(9)
+
+
+def sum_list(arr):
+    if len(arr) == 0:
+        return 0
+    return arr[0] + sum_list(arr[1:])
+
+sum_l= sum_list([2, 4, 6])
+print("sum is: ", sum_l)
+
+
+def mystery(n):
+    if n == 0:
+        return
+    mystery(n-1)
+    print(n)
+mystery(3)
