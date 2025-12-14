8ddcd7b3ceacca1e5051e51cf0293d59a0002e95
Shiva Nagendra <shivanagendra01@gmail.com>
2025-12-14 13:21:32 +0530

    Day 18: Quick Sort implementation and recursion boundaries practice

---
+++ day_18.py
@@ -0,0 +1,23 @@
+#Quick sort
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range (low, high):
+        if arr[j]<pivot:
+            i+=1
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+    return i+1
+
+def quick_sort(arr, low, high):
+    if low<high:
+        pivot_index = partition(arr, low, high)
+        quick_sort(arr, low, pivot_index - 1)
+        quick_sort(arr,  pivot_index+1,high)
+    
+nums = [8, 3, 5, 2, 9, 1]
+quick_sort(nums, 0, len(nums) - 1)
+print(nums)
+
+
