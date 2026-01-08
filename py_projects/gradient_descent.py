7643534c7e6592352cff53ae46908867cfbaf345
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-09 00:04:32 +0530

    Week 5 project: gradient descent from scratch

---
+++ py_projects/gradient_descent.py
@@ -0,0 +1,25 @@
+#gradient_descent from scratch
+
+import random
+
+def f(x):
+    return x**2
+def df(x):
+    return x*2
+
+x = random.uniform(-10, 10)
+learning_rate = 0.1
+steps = 20
+
+print(f"starting x:{x}\n")
+
+for i in range(steps):
+    grad = df(x)
+    x = x - learning_rate*grad
+    y = f(x)
+
+    print(f"step {i+1}: x = {x:.3f}, y = {y:.3f}")
+
+
+
+   
