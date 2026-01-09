f178dd0a56bd3a3fe5d7e8dc55cb0ec8e9de11c8
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-09 15:05:43 +0530

    Week 5 project: gradient descent from scratch

--- py_projects/gradient_descent.py
+++ py_projects/gradient_descent.py
@@ -1,6 +1,10 @@
 #gradient_descent from scratch
 
+#gradient_descent from scratch
+
 import random
+import matplotlib.pyplot as plt
+
 
 def f(x):
     return x**2
@@ -13,9 +17,31 @@ steps = 20
 
 print(f"starting x:{x}\n")
 
+xs = []
+ys = []
+
 for i in range(steps):
     grad = df(x)
     x = x - learning_rate*grad
     y = f(x)
 
+    xs.append(x)
+    ys.append(y)
+
     print(f"step {i+1}: x = {x:.3f}, y = {y:.3f}")
+
+#plotting parabola   
+import numpy as np
+x_curve = np.linspace(-6,6,100)
+y_curve = f(x_curve)
+plt.plot(x_curve, y_curve)
+
+#plot gradient decent path
+plt.scatter(xs,ys)
+plt.plot(xs,ys)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Gradient decent on y = x**2")
+plt.grid(True)
+
+plt.show()
