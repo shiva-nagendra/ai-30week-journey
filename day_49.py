edf49b2c51a5f56a6d456858afddc0ac6d969bf6
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-10 21:33:02 +0530

    Day 49: numpy practice

--- day_49.py
+++ day_49.py
@@ -9,7 +9,12 @@ print("b = ",B)
 print("B + 10 = ", B + 10)
 print("B * 2 = ", B * 2)
 
-C = np.array([10,20,30])
+C = np.array([10,20,30]) #vector addition
 print("B + C =", B + C)
 
-print("dot product:", np.dot(B, C))
+print("dot product:", np.dot(B, C)) #dot product
+
+d = np.array([43,44,55,66])
+e= np.array([3,2,4,2])
+
+print(np.dot(d,e))
