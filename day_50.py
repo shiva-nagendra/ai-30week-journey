d18d76609ba7094c9554f28a5ac97ea25c20cf08
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-10 22:04:26 +0530

    Day 50: 2D matrices practice

---
+++ day_50.py
@@ -0,0 +1,30 @@
+#2D matrices practice
+
+import numpy as np
+
+A = np.array([ #2x3 matrix
+    [1,2,3],
+    [4,5,6]
+])
+
+v = np.array([10,20,30])
+
+print("mat A:",A)
+print("vec v:",v)
+
+res = A.dot(v)
+print("A.v : \n", res)
+
+B = np.array([ #3x2 matrix
+    [2,3],
+    [3,4],
+    [4,5]
+])
+
+C = A.dot(B)
+print("B matrix: ", B)
+print("A.B : \n", C)
+
+print("\nShape of A:", A.shape)
+print("\nShape of B:", B.shape)
+print("\nShape of C:", C.shape)
