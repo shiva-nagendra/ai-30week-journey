a1582f7d585416857e7bd532ce70f7edd69ac3e8
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-11 12:42:22 +0530

    Day 51: NumPy broadcasting and vectorized operations

--- day_51.py
+++ day_51.py
@@ -7,6 +7,8 @@ data = np.array([1,2,3,4,5])
 A = data * 2
 print("raw data:", data)
 print("scaled data:", A)
+shifted = data + 5
+print("\nshifted data:",shifted)
 
 B = np.array([
     [2, 3, 4],
@@ -14,10 +16,10 @@ B = np.array([
     [4, 5, 6]
 ])
 
-v = np.array([10,20,30])
+v = np.array([10,20,30]) #add vector to each row
 print("\ndata of B:\n", B)
 print("\ndata B + v:\n",B+v)
 print("\ndivide all values:\n",B/10)
 
-c = B.dot(v)
+c = B.dot(v) #divide values
 print("\nB.dot(c) :\n",c)
