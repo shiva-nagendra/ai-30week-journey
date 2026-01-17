3353e8002c57a5245ec874b6c091c7d024e3b7c7
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-17 18:36:44 +0530

    Day 58: Pandas merging and joining DataFrames

---
+++ day_58.py
@@ -0,0 +1,17 @@
+#merging on pandas
+
+import pandas as pd
+
+employees = pd.read_csv("employees.csv")
+scores = pd.read_csv("scores.csv")
+
+print("\nEmployees: \n", employees)
+print("\nScores: \n", scores)
+
+#merge function
+
+merged = pd.merge(employees, scores, on="emp_id")
+print("\nMerged data: \n", merged)
+
+avg_scores = merged.groupby("department")["score"].mean()
+print("\n Average score : \n", avg_scores)
