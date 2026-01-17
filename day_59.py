8d4ada48ebbd71441a2308fcee756e613c3a2f9d
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-17 19:12:59 +0530

    Day 59: Pandas mini-project with merge, cleaning, and group analysis

--- day_59.py
+++ day_59.py
@@ -13,7 +13,14 @@ print("\nScores:\n",scores)
 merged_data = pd.merge(employees,scores,on="emp_id")
 print("\nMerged data: \n", merged_data)
 
+# check missing data
+print("\nMissing data, \n", merged_data.isnull())
+
+
+#fill missing score with avg
 merged_data["score"] = merged_data["score"].fillna(merged_data["score"].mean())
+
+print("\nfilled data: \n", merged_data)
 
 #average score by dept.
 
