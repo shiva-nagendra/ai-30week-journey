d7855c04ef75a8956a48727b28babff197dffa0d
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-17 18:41:14 +0530

    Day 58: Pandas merging and joining DataFrames

--- day_58.py
+++ day_58.py
@@ -13,6 +13,10 @@ print("\nScores: \n", scores)
 merged = pd.merge(employees, scores, on="emp_id")
 print("\nMerged data: \n", merged)
 
+#sort values
+sorted_values = merged.sort_values(by="score", ascending=True)
+print("\nSorted values: \n",sorted_values)
+
 #average scores
 avg_scores = merged.groupby("department")["score"].mean()
 print("\n Average score : \n", avg_scores)
