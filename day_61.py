3e112988d511eab760c01e5608749c1d1ad1d406
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-20 08:23:32 +0530

    Day 61: EDA correlation analysis and feature relationships

---
+++ day_61.py
@@ -0,0 +1,46 @@
+#correlation practice
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("day_60.csv")
+print("\nData:\n",df)
+
+#correlation matrix
+corr = df.corr(numeric_only=True)
+print("\nCorrelation matrix\n",corr)
+
+#correlation score
+score_corr = corr["score"].sort_values(ascending=False)
+print("\nscore correlation:\n",score_corr)
+
+#scatter plots with strongest relationship
+
+#score vs study hours
+plt.figure()
+plt.scatter(df["score"],df["study_hours"])
+plt.title("score vs study_hours")
+plt.xlabel("score")
+plt.ylabel("study_hours")
+plt.show()
+
+#score vs age
+plt.figure()
+plt.scatter(df["score"],df["age"])
+plt.xlabel("score")
+plt.ylabel("age")
+plt.title("Age vs Score")
+plt.show()
+
+#score vs income
+plt.figure()
+plt.scatter(df["score"],df["income"])
+plt.xlabel("Score")
+plt.ylabel("income")
+plt.title("Score vs income")
+plt.show()
+
+# git status
+# git add .
+# git commit -m "Day 61: EDA correlation analysis and feature relationships"
+# git push
