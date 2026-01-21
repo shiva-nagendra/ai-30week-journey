b9f71e97d5395707cd169303c7ca884aacc43285
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-22 00:08:35 +0530

    Day 63: EDA skewness detection and feature transformation

--- day_63.py
+++ day_63.py
@@ -16,7 +16,6 @@ plt.xlabel("Income")
 plt.ylabel("Frequency")
 plt.show()
 
-
 #log transformation (fix scew)
 df["income_log"] = np.log(df["income"])
 
@@ -27,7 +26,7 @@ print(df[["income","score"]].corr())
 print("\ncorrelation with score(log income):\n")
 print(df[["income_log","score"]].corr())
 
-
+#log distribution
 plt.figure()
 plt.hist(df["income_log"], bins=6)
 plt.title("income(log transformed)")
