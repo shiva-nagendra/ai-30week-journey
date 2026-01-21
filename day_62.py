3a183ba01d8b909ff0f245b8e6867c835f9b37dc
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-21 18:36:18 +0530

    Day 62: Outliers, Skewness & Data Issues (EDA reality check)

---
+++ day_62.py
@@ -0,0 +1,15 @@
+#Outliers, Skewness & Data Issues (EDA reality check)
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("day_62.csv")
+print("\nData:\n",df) 
+
+#box plot to dataset outliers
+
+plt.figure()
+plt.boxplot(df["score"])
+plt.title("Score Boxplot")
+plt.ylabel("Score")
+plt.show()
