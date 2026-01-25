3faab16ce07d4cf90d8be678353aea33e16342ca
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-25 19:34:04 +0530

    week 10 day 1 : Matplotlib Basics: Visualizing EDA Insights

---
+++ day_65.py
@@ -0,0 +1,29 @@
+#week 10 day 1 
+#Matplotlib Basics: Visualizing EDA Insights
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#load data
+df = pd.read_csv("telecom_churn.csv")
+
+#convert total charges safely
+df["TotalCharges"]=pd.to_numeric(df["TotalCharges"],errors="coerce")
+df.dropna(subset=["TotalCharges"])
+
+#Churncount plot
+churn_counts = df["Churn"].value_counts()
+
+plt.figure()
+plt.bar(churn_counts.index, churn_counts.values)
+plt.title("Monthly charges by churn")
+plt.suptitle("")
+plt.xlabel("Churn")
+plt.ylabel("Monthly charges")
+plt.show()
+
+#Month;y charges by churn (boxplot)
+plt.figure()
+plt.boxplot(column="tenure", by="Churn")
+
+
