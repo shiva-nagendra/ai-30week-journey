d475f650f0246f50d12d98a48982e9e1e80fb63a
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-26 14:21:48 +0530

    Week 10 Day 2: Seaborn categorical plots for churn analysis

---
+++ day_66.py
@@ -0,0 +1,18 @@
+#Seaborn(smart drawing for data) + Categorical Comparisons (Seeing churn patterns clearly)
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#load data
+df = pd.read_csv("telecom_churn.csv")
+
+#clean total charges
+df["TotalCharges"] = pd. to_numeric(df["TotalCharges"], errors="coerce")
+df = df.dropna(subset=["TotalCharges"])
+
+#contract vs churn
+
+sns.countplot(data=df, x="Contract", hue="Churn")
+plt.title("Churn by Contract type")
+plt.show()
