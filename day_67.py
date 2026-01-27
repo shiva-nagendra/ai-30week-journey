b9074c20b4f3982050534fe82651fb98272415a6
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-27 22:21:37 +0530

    Week 10 Day 67: Seaborn numerical distributions and churn comparison

---
+++ day_67.py
@@ -0,0 +1,17 @@
+#Numerical Distributions with Seaborn 
+
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#load data
+df = pd.read_csv("telecom_churn.csv")
+
+df["TotalCharges"]= pd.to_numeric(df["TotalCharges"],errors="coerce")
+df = df.dropna(subset=["TotalCharges"])
+
+#Distribution of monthly charges
+sns.histplot(data=df, x="MonthlyCharges",bins=10, kde=True)
+plt.title("Distribution of Monthly charges")
+plt.show()
+
