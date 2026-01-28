3c249930f25180cd8072c00268dd942e6f08e67f
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-28 16:13:41 +0530

    Week 10 Day 68: Correlation heatmap and feature relationship analysis

--- day_68.py
+++ day_68.py
@@ -10,9 +10,11 @@ df = pd.read_csv("telecom_churn.csv")
 df["TotalCharges"]= pd.to_numeric(df["TotalCharges"], errors="coerce")
 df= df.dropna(subset=["TotalCharges"])
 
+
 corr = df.corr(numeric_only=True)
 print(corr)
 
+#correlatiion heatmap
 plt.figure(figsize=(6,4))
 sns.heatmap(corr, annot=True, cmap="coolwarm")
 plt.title("Correlation heatmap")
