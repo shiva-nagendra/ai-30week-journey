143debd7a8cdde2e02a26bc2eefd153ce2a1ba15
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-28 16:09:08 +0530

    Week 10 Day 68: Correlation heatmap and feature relationship analysis

---
+++ day_68.py
@@ -0,0 +1,15 @@
+#Correlation Heatmaps & Feature Relationships
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#load data
+df = pd.read_csv("telecom_churn.csv")
+
+df["TotalCharges"]= pd.to_numeric(df["TotalCharges"], errors="coerce")
+df= df.dropna(subset=["TotalCharges"])
+
+corr = df.corr(numeric_only=True)
+print(corr)
+
