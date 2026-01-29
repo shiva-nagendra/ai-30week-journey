e63537f12a1c28d928cd1e23e8d9cb10d185b19c
Shiva Nagendra <shivanagendra01@gmail.com>
2026-01-29 12:26:45 +0530

    Week 10 Day 69: Visualization mini-project for telecom churn analysis

--- day_69.py
+++ day_69.py
@@ -3,19 +3,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #load data
-
 df = pd.read_csv("telecom_churn.csv")
 df["TotalCharges"] = pd.to_numeric(df["TotalCharges"],errors="coerce")
 df = df.dropna(subset=["TotalCharges"])
 
 #churn distrubution 
-
 sns.countplot(data=df,x="Churn")
 plt.title("Churn Distribution")
 plt.show()
 
 #Contract vs churn
-
 sns.countplot(data=df, x="Contract", hue="Churn")
 plt.title("Contract vs Churn")
 plt.show()
