9e8760577fac4ac24e22274640a90b38f72f3698
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-01 19:55:24 +0530

    Day 71: installing Scikit learn and theory handling

--- day_71.py
+++ day_71.py
@@ -3,5 +3,7 @@
 from sklearn.linear_model import LinearRegression
 
 model = LinearRegression()
-model.fit(X_train, y_train)
-predictions = model.predict(X_test)
+
+#model.fit(X_train, y_train)
+
+#predictions = model.predict(X_test)
