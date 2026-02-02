49161f4dcde65e307d287f07b4eb886bd13a1673
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-02 18:34:46 +0530

    Day 72: Theory: Supervised vs unsupervised learning

---
+++ day_72.py
@@ -0,0 +1,9 @@
+#machine learning basics
+
+#supervised vs unsupervised learning
+
+from sklearn.linear_model import LinearRegression
+
+model = LinearRegression()
+#model.fit(x, y) #supervised: x has answers y
+
