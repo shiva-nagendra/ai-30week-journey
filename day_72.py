e91ce9ae5fc39b8dd7b759f950c32cc04a03a3cd
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-02 18:37:06 +0530

    Week 11 Day 2: Supervised vs unsupervised learning concepts

--- day_72.py
+++ day_72.py
@@ -1,4 +1,4 @@
-#machine learning basics
+#machine learning basics #Theory
 
 #supervised vs unsupervised learning
 
@@ -7,3 +7,12 @@ from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 #model.fit(x, y) #supervised: x has answers y
 
+# Unsupervised is for:
+# 	•	exploration
+# 	•	segmentation
+# 	•	anomaly detection
+
+# Supervised is for:
+# 	•	prediction
+# 	•	decision-making
+# 	•	business impact
