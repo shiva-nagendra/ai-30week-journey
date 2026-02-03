8f8d65c38736147dbc93aa4ed9bc4545dc130d26
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-03 15:17:20 +0530

    Week 11 Day 73: First linear regression model with scikit-learn

---
+++ day_73.py
@@ -0,0 +1,24 @@
+#Linear regression
+
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+#House size(sq.ft)
+x = np.array([500, 600, 1000, 1200, 1500]).reshape(-1, 1)
+
+#House price in lakhs
+y = np.array([30,50,65,80,100])
+
+#Create model
+model = LinearRegression()
+
+#Train model
+model.fit(x, y)
+
+#make prediction
+new_size = np.array([[1800]])
+predicted_price = np.round(model.predict(new_size),2)
+
+
+print("\nPredicted price for 1800 sq.ft. house:\n", predicted_price,"lakhs")
+
