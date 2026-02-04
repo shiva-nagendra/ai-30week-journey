160f0d5b1abff1338e24f1c245836450497d0882
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-04 11:55:40 +0530

    Day 74: Machine learning: Train-split data

---
+++ day_74.py
@@ -0,0 +1,24 @@
+#train test split
+
+#training data = model learns patterns
+#testing data = model is evaluated honestly
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+#dataset
+x = np.array([500,600,1000,1200,1500,1800,2000]).reshape(-1,1)
+y = np.array([30,38,65,80,100,120,135])
+
+
+#split data
+x_train, x_test, y_train, y_test =train_test_split(
+    x, y, test_size=0.3, random_state=42
+)
+
+#create model
+model = LinearRegression()
+
+#train on training data
+
