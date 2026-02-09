001a2ba59b771606516bc43a18c5e47c6c82a97e
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-09 11:18:03 +0530

    Week 12 Day 6: ROC curve and AUC for binary classification

--- day_81.py
+++ day_81.py
@@ -18,7 +18,7 @@ y = df["target"]
 
 #split train and test
 x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.5, random_state=42
+    x, y, test_size=0.3, random_state=42
 )
 
 #model train
