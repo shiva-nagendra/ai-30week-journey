d7fd96c636c12b325dd66ebb9b718eb482df0922
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-10 00:57:31 +0530

    Week 13 Day 4: Normalization vs Standardization and outlier behavior

---
+++ day_84.py
@@ -0,0 +1,16 @@
+#week 13 day 4
+#standardization vs normalization
+
+import numpy as np
+from sklearn.preprocessing import StandardScaler,MinMaxScaler
+
+x = np.array([[10],[20],[30],[1000]])
+
+std = StandardScaler()
+norm = MinMaxScaler()
+
+print("Standardized:")
+print(np.round(std.fit_transform(x),2))
+
+print("\nNormalized:")
+print(np.round(norm.fit_transform(x),2))
