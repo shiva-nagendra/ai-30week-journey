c0362ca97acf5e3922148117a072266b120c17c4
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-11 15:39:17 +0530

    Week 13 Day 5: Basic Decision Tree Classifier (no scaling required)

---
+++ day_85.py
@@ -0,0 +1,39 @@
+#week 13 day 5
+#Decision tree 
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+#load data
+data = load_breast_cancer(as_frame=True)
+df = data.frame
+
+#split features and target
+x = df.drop(columns = ["target"])
+y = df["target"]
+
+#split train and test
+x_train,x_test,y_train,y_test = train_test_split(
+    x,y,test_size=0.3,random_state=42
+)
+
+#model selection
+model_lr = LogisticRegression(max_iter=2345)
+model_lr.fit(x_train,y_train)
+
+model_tree = DecisionTreeClassifier(random_state=42)
+model_tree.fit(x_train,y_train)
+
+#model predict
+y_pred_lr = model_lr.predict(x_test)
+y_pred_tree = model_tree.predict(x_test)
+
+#evaluation
+acc_lr = accuracy_score(y_test,y_pred_lr)
+acc_tree = accuracy_score(y_test,y_pred_tree)
+
+print("\nLogisitic accuracy:",acc_lr)
+print("\nTree model accuracy:",acc_tree)
