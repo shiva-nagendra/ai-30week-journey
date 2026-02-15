e791296ab865fca9fe9ccbc9447da1822ad55665
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-16 01:49:33 +0530

    Week 14 Day 5: Naive Bayes classification

--- day_91.py
+++ day_91.py
@@ -16,4 +16,10 @@ df = data.frame
 x = df.drop(columns=["target"])
 y = df["target"]
 
+#train test split
+x_train,x_test,y_train,y_test = train_test_split(
+    x,y,test_size=0.3,random_state=42
+)
+
+
 
