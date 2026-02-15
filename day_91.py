27b54c2b314a14a53168fd261265d7facb1c9b82
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-16 01:48:31 +0530

    Week 14 Day 5: Naive Bayes classification

---
+++ day_91.py
@@ -0,0 +1,11 @@
+#week 14 day 5
+#Naive Bayes
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+
+
