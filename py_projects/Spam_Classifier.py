6ba745c1d34673e60914a53ecd285a336294d45b
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-17 12:32:09 +0530

    Week 14 Project: Full Spam Classifier Benchmark (RF vs NB vs SVM)

---
+++ py_projects/Spam_Classifier.py
@@ -0,0 +1,43 @@
+#Week 14 project: Spam Classifier
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score,precision_score,f1_score,recall_score
+
+#Load Data
+df = pd.read_csv("spam.csv")
+
+#convert labels to numeric
+df["label"] = df["label"].map({"ham": 0}, {"spam" : 1})
+
+x = df["messege"]
+y = df["label"]
+
+#train test split
+x_train, x_test, y_train, y_test = train_test_split(
+    x,y,test_size=0.3,random_state=42
+)
+
+#models dict
+models = {
+    "Random Forest" : Pipeline([
+        ("vectorizer", CountVectorizer()),
+        ("model",RandomForestClassifier(random_state=42))
+    ]),
+
+    "Naive Bayes" : Pipeline([
+        ("vectorizer", CountVectorizer()),
+        ("model", MultinomialNB())
+    ]),
+
+    "SVM (linear)" : Pipeline([
+        ("vectorizer", CountVectorizer()),
+        ("model", LinearSVC())
+    ])
+}
+
