d624552c137174223684cab882b4efd4c5679595
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-20 01:27:32 +0530

    Week 15 Project: Customer segmentation using K-Means clustering

---
+++ day_96.py
@@ -0,0 +1,9 @@
+#week 15 day 5
+#cust segmentation project p-1
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
