fe67573aa95cdb6a179f67aa01e44c5138e268d3
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-23 21:20:37 +0530

    Week 15 Project: Customer segmentation using KMeans with elbow, silhouette and cluster interpretation

--- py_projects/Customer_segmentation.py
+++ py_projects/Customer_segmentation.py
@@ -2,6 +2,7 @@
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -17,4 +18,25 @@ x = df[["Annual Income (k$)","Spending Score (1-100)"]]
 #Scaling
 scaler = StandardScaler()
 x_scaled = scaler.fit_transform(x)
+
+#Elbow visualization
+inertia_val = []
+
+for k in range(1, 11):
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(x_scaled)
+    inertia_val.append(kmeans.inertia_)
+
+plt.plot(range(1, 11), inertia_val, marker = "o")
+plt.title("Elbow K")
+plt.xlabel("")
+plt.ylabel("")
+plt.show()
+
+#Apply k means
+#Elbow
+
+for k in range(1, 11):
+    kmean = KMeans(n_clusters=k,random_state=42)
+    clusters = kmean.fit_predict(x_scaled)
 
