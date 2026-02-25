4a83f951f770fb65470e3890b79fc95250477eb3
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-25 11:41:27 +0530

    Week 16 Day 4: PCA Scree plot and cumulative variance analysis for component selection

--- day_100.py
+++ day_100.py
@@ -1,16 +1,17 @@
 #Week 16 day 4
 #Scree plot and choosing PCA    
 
-from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 #Load data
 df = pd.read_csv("Mall_Customers.csv")
 
-#Gendr encode and x
-df["Gender"] = df["Gender"].map({"Male":0,"Female":1})
+#Gender Encode & x
+df["Gender"] = df["Gender"].map({"Male":0, "Female":1})
 
 x = df[["Gender","Age","Annual Income (k$)", "Spending Score (1-100)"]]
 
@@ -18,3 +19,17 @@ x = df[["Gender","Age","Annual Income (k$)", "Spending Score (1-100)"]]
 scaler = StandardScaler()
 x_scaled = scaler.fit_transform(x)
 
+#Apply PCA
+pca = PCA()
+x_pca = pca.fit_transform(x_scaled)
+
+#Explained variance
+x_var = pca.explained_variance_ratio_
+print(x_var)
+
+#scree plot
+plt.plot(range(1, len(x_var)+1), x_var, marker = "o")
+plt.title("Scree plot")
+plt.xlabel("Principal componenets")
+plt.ylabel("Variance Explained")
+plt.show()
