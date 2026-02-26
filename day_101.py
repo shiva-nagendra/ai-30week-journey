881da893848f909e878768f13c39e509662412f1
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-26 22:48:11 +0530

    Week 16 Day 5: PCA visualization of KMeans customer segments

---
+++ day_101.py
@@ -0,0 +1,9 @@
+#week 16 day 5
+# PCA + Cluster visualisation
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import pandas as pd
+
