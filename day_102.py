b6b0263f1e5b51a1d0cb81f130e9e6519939c4b9
Shiva Nagendra <shivanagendra01@gmail.com>
2026-02-27 16:21:48 +0530

    Week 16 Day 6: PCA + KMeans pipeline and cluster interpretation

---
+++ day_102.py
@@ -0,0 +1,11 @@
+#week 16 Day 6
+# PCA + Kmeans pipeline
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Load data
+
