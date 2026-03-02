66357d3a37d65fbfe5862313af1cb1773160a2d7
Shiva Nagendra <shivanagendra01@gmail.com>
2026-03-02 15:43:12 +0530

    Week 17 Day 4: Implemented full PyTorch training loop with MSE loss and SGD optimizer

--- day_105.py
+++ day_105.py
@@ -1,48 +1,60 @@
 #week 17 day 4
+# Training loop
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
-# Fake dataset (y = 2x + 1)
+#Create fake data
 x = torch.randn(100, 1)
-y = 2 * x + 1
+y = 2*x + 1
 
+#Define model
 
-# Define simple linear model
 class SimpleModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.linear = nn.Linear(1, 1)
+        self.linear = nn.Linear(1,1) #1 input 1 output
 
-    def forward(self, x):
+    def forward(self,x):
         return self.linear(x)
     
-# Create model
 model = SimpleModel()
 
-# Define loss function
-criterion = nn.MSELoss()
+#Define loss function
 
-# Define optimizer
+criterion = nn.MSELoss() #mean squared error
+
+#Define optimizer
+
 optimizer = optim.SGD(model.parameters(), lr=0.1)
 
-# Training loop
+#trainig loop
 for epoch in range(50):
 
-    # Forward pass
+    #forward pass (model make prediction)
     predictions = model(x)
-    loss = criterion(predictions, y)
 
-     # Backward pass
+    #compute loss(measure error)
+    loss = criterion(predictions,y)
+
+    #Clear old gradients
     optimizer.zero_grad()
+
+    #Backward pass(compute gradients)
     loss.backward()
 
-    # Update weights
+    #update weights
     optimizer.step()
 
+    #print progress
     if epoch % 10 == 0:
-        print(f"Epoch {epoch}, Loss: {loss.item()}")
-# Check learned parameters
-for name, param in model.named_parameters():
-    print(name, param.data)
+        print(f"Epoch: {epoch}, loss: {loss.item()}")
+
+#check learned parameters
+
+print("\nLearned parameters:")
+for name, params in model.named_parameters():
+    print(name, params.data)
+    
+    
