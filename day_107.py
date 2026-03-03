3b0bffce29d3bde331dccf10abd44c6e04e5922e
Shiva Nagendra <shivanagendra01@gmail.com>
2026-03-03 12:49:51 +0530

    Week 17 Day 6: Implemented multi-class ANN on Iris dataset

---
+++ day_107.py
@@ -0,0 +1,24 @@
+#week 17 day 6
+#Training a Neural Network on a Real Dataset (Iris)
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+#load data
+iris = load_iris()
+x = iris.data
+y = iris.target
+
+#split data
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, random_state=42, test_size=0.2
+)
+
+#scale features
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
