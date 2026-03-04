a5938d0d4f67da89423a2045ef4f22585a6fdbcd
Shiva Nagendra <shivanagendra01@gmail.com>
2026-03-04 19:35:25 +0530

    Week 17 Project: Iris classifier using PyTorch neural network

--- py_projects/ANN_iris_Clarifier.py
+++ py_projects/ANN_iris_Clarifier.py
@@ -18,3 +18,8 @@ y = iris.target
 x_train, x_test, y_train, y_test = train_test_split(
     x,y,test_size=0.2,random_state=42
 )
+
+#feature scaling
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
