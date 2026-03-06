881fca3488f91d18f7f2ef02a823cbb78ec6a398
Shiva Nagendra <shivanagendra01@gmail.com>
2026-03-06 15:55:44 +0530

    Week 18 Day 2: Built CNN architecture with Conv, Pool, Flatten, Linear

--- day_109.py
+++ day_109.py
@@ -15,4 +15,15 @@ class CNNmodel(nn.Module):
             kernel_size=3
         )
 
+        self.pool = nn.MaxPool2d(2)
+
+        #second convolution block
+        self.conv2 = nn.Conv2d(
+            in_channels=8,
+            out_channels=16,
+            kernel_size=3
+        )
+
+        
+
      
