3ee5c7043e0a0bce9fc3f70f789153bd7284c72a
Shiva Nagendra <shivanagendra01@gmail.com>
2026-03-07 22:54:44 +0530

    Week 18 Day 3: Trained CNN on MNIST handwritten digit dataset

--- day_110.py
+++ day_110.py
@@ -5,5 +5,27 @@ import torch
 import torch.nn
 import torch.optim as optim
 from torchvision import datasets, transforms
-from torch.utils.data import dataloader
+from torch.utils.data import DataLoader
+
+#Transform images to tensors
+transform_tensor = transforms.ToTensor()
+
+#Load MNIST dataset
+train_dataset = datasets.MNIST(
+    root="./data",
+    download=True,
+    train=True,
+    transform=transform_tensor
+)
+
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=64,
+    shuffle=True
+)
+
+    
+
+
+
 
