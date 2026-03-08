b38b51e7cf94013a8597ed63d9e32a3eb9fb6603
Shiva Nagendra <shivanagendra01@gmail.com>
2026-03-08 19:19:40 +0530

    Week 18 Day 5: Evaluate CNN on MNIST test dataset and compute accuracy

---
+++ day_111.py
@@ -0,0 +1,8 @@
+#week 18 day 5
+# Evaluate CNN on MNIST test set
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
