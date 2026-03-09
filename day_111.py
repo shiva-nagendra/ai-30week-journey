db856819e543b5692ee492cb1e692dc7c25aac17
Shiva Nagendra <shivanagendra01@gmail.com>
2026-03-09 16:01:28 +0530

    Week 18 Day 5: Evaluate CNN on MNIST test dataset and compute accuracy

--- day_111.py
+++ day_111.py
@@ -37,3 +37,24 @@ test_loader = DataLoader(
     shuffle=False
 )
 
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(1,8,3)
+        self.pool = nn.MaxPool2d(2)
+
+        self.conv2 = nn.Conv2d(8,16,3)
+        self.fc = nn.Linear(16*5*5,10)
+
+    def forward(self,x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+
+        x = torch.flatten(x,1)
+
+        x = self.fc(x)
+
+        return x
+    
+
