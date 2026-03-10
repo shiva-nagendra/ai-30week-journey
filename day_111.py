00ee0fc059844349fbd4cab84151f98e217a3611
Shiva Nagendra <shivanagendra01@gmail.com>
2026-03-10 22:19:01 +0530

    Week 18 Day 5: Evaluate CNN on MNIST test dataset and compute accuracy

--- day_111.py
+++ day_111.py
@@ -6,3 +6,13 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+
+
+transform = transforms.ToTensor()
+
+# training dataset
+train_dataset = datasets.MNIST(
+    root="./data",
+    train=True,
+    download=False,
+    transform=transform
