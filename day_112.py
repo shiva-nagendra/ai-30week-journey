6275b88705cfa32bc5dcfd54118d6891e00e9732
Shiva Nagendra <shivanagendra01@gmail.com>
2026-03-11 20:21:50 +0530

    Week 18 Day 6: Save and load trained CNN model using PyTorch

---
+++ day_112.py
@@ -0,0 +1,9 @@
+# Week 18 Day 6
+# Save and load CNN model
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
