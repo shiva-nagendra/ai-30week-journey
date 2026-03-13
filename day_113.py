932b5700a16ac0b8c0416fba86de14d5597b5da1
Shiva Nagendra <shivanagendra01@gmail.com>
2026-03-13 20:07:14 +0530

    Week 19 Day 1: First HuggingFace Transformer pipeline for sentiment analysis

---
+++ day_113.py
@@ -0,0 +1,9 @@
+#week 19 #Day 1 
+#Transformers #Hugging face sentiment analysis
+
+from transformers import pipeline
+sentiment = pipeline("sentiment-analysis")
+print(sentiment("I love learning AI"))
+
+
+
