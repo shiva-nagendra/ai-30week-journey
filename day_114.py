c47d05a580248bf5c3d061c9d6c5a194efecf3f8
Shiva Nagendra <shivanagendra01@gmail.com>
2026-03-14 11:02:54 +0530

    Week 19 Day 2: HuggingFace transformer text summarization pipeline

--- day_114.py
+++ day_114.py
@@ -8,40 +8,21 @@ summerizer = pipeline("summarization",
                       )
 
 text = """
-### Me 🌟📚💼
-Rajitha Pathipaka, based in Hyderabad, Telangana, is a dedicated professional with a B.Tech from Jyotishmathi Institute of Technological Sciences and a Certified Data Analyst from Datamites Hyderabad. With experience as a Transaction Associate II at Concentrix from March 2020 to February 2023, I have honed my skills in delivering exceptional customer service, resolving complex issues, and working independently in fast-paced environments.
-
-### Projects 🚀🔍📊
-#### Project on Medical Data History 🩺💻
-- **Objective:** Develop a user-friendly system using SQL to manage and analyze medical records.
-- **Details:** Engineered a secure database for storing patient data, integrated SQL queries for easy access by healthcare professionals, and developed SQL-based tools for data analysis, aiding in informed decision-making for patient care.
-
-#### HR Analytics Initiative 📈👥💡
-- **Objective:** Improve workforce management using MS Excel and Tableau.
-- **Details:** Conducted data cleansing and modeling in Excel to analyze employee performance, retention, and recruitment trends. Utilized Tableau for visualization, creating interactive dashboards for monitoring key HR metrics. Contributed to informed decision-making by optimizing recruitment strategies and enhancing employee satisfaction through data-driven insights.
-
-### Technical Skills and Others 🛠️💡🎨
-- **Data Analysis:** Proficient in SQL, Python, and MS Excel.
-- **Data Visualization:** Skilled in Power BI, Tableau, and MS Excel for creating visually appealing dashboards.
-- **Problem Solving:** Strong attention to detail and analytical mindset.
-- **Customer Service:** Committed to delivering exceptional service and resolving complex issues promptly.
-
-### Achievements & Awards 🏆🥇
-- Recognized as "Best Employee" twice.
-
-### Personal Details 📅👤🌍
-- **Date of Birth:** July 29, 1997
-- **Marital Status:** Single
-- **Nationality:** Indian
-- **Gender:** Female
-- **Languages Known:** English, Hindi, Telugu
-
+ Good knowledge of using find, sed command
+Expertise in using Unix Utilities and associated command with Oracle Database
+ Scheduling Backup and Backup scripts using CRONTAB Scheduled Jobs.
+ Scheduling job by at and crontab feature
+ Use a regular expression to search for multiple similar strings
+ Hands-on expertise in SQL statements (DDL, DML, DCL, TCL).
+ Good Knowledge of all types of constraints.
+ Good Knowledge of sequence in SQL
+ Working Knowledge of Jira ticketing tool
 """
 
 summerize = summerizer(
     text,
-    max_length=30,
-    min_length=10,
+    max_length=60,
+    min_length=30,
     do_sample=False)
 
 print("\nsummerized text:",summerize)
