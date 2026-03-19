fdc6097bdb4633d3c6b8fac8838fc32583adf9d7
Shiva Nagendra <shivanagendra01@gmail.com>
2026-03-19 21:27:27 +0530

    Week 19 Project: Semantic search engine with top-k retrieval using embeddings

---
+++ py_projects/Semantic_search_engine.py
@@ -0,0 +1,7 @@
+#week 19 project
+# Semantic search engine(Top-k retrieval)
+
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
