7efafefe58debe39aa328cdc2a474998445e1ad7
Shiva Nagendra <shivanagendra01@gmail.com>
2026-04-02 22:26:22 +0530

    Week 20 Project: Manual transformer pipeline for sentiment analysis

--- py_projects/Transformer_pipeline.py
+++ py_projects/Transformer_pipeline.py
@@ -5,3 +5,7 @@ import os
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
 
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import numpy as np
+
