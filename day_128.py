12f8059d305b3442cd582d2c94741c4a81307f43
Shiva Nagendra <shivanagendra01@gmail.com>
2026-04-06 21:30:44 +0530

    Week 21 Day 4: Vector database using FAISS

--- day_128.py
+++ day_128.py
@@ -10,5 +10,11 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 
 #Documents
 documents = [
-    
+    "AI helps healthcare",
+    "Machine learning analyzes data",
+    "Rockets go to space",
+    "Deep learning improves vision",
+    "Cooking requires ingredients"
 ]
+
+
