809da5e036a930754a49cd5131b784ac2399d631
Shiva Nagendra <shivanagendra01@gmail.com>
2026-04-06 21:30:22 +0530

    Week 21 Day 4: Vector database using FAISS

---
+++ day_128.py
@@ -0,0 +1,14 @@
+#week 21 day 4
+#Vector database with FAISS
+
+from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
+
+#Load model
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+#Documents
+documents = [
+    
+]
