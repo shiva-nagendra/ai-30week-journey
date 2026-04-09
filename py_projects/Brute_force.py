17a20bc294787e8d9c1fcd640222bf3cab53f897
Shiva Nagendra <shivanagendra01@gmail.com>
2026-04-09 21:48:12 +0530

    Week 21 Project: Semantic Search Engine using embeddings and cosine similarity

---
+++ py_projects/Brute_force.py
@@ -0,0 +1,7 @@
+#week 21 project
+#Semantic search engine (BRUTE Force)
+
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
