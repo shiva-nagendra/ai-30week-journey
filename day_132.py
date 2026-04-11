dafc78c9951ebea2cc90d4b4cc21bcdf04227d63
Shiva Nagendra <shivanagendra01@gmail.com>
2026-04-11 13:00:25 +0530

    Week 22 Day 2: Implemented FAISS vector search

--- day_132.py
+++ day_132.py
@@ -8,3 +8,16 @@ import faiss
 #load model
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
+
+# Data
+sentences = [
+    "AI is transforming healthcare",
+    "Machine learning analyzes data",
+    "Deep learning improves vision",
+    "Space rockets explore planets",
+    "Healthy food improves life",
+    "Exercise keeps the body fit",
+    "Solar energy is renewable",
+    "Wind power generates electricity"
+]
+
