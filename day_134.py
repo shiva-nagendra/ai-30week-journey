60d6536fc1f9099356e342455b2e9df69a9d4ac5
Shiva Nagendra <shivanagendra01@gmail.com>
2026-04-14 15:43:33 +0530

    Week 22 Day 4: FAISS with cosine similarity using normalization

---
+++ day_134.py
@@ -0,0 +1,26 @@
+#week 22 day 4
+#FAISS with cosine similarity
+
+from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
+
+#Load model
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+#sentences
+sentences = [
+    "AI is transforming healthcare",
+    "Machine learning analyzes data",
+    "Deep learning improves vision",
+    "Space rockets explore planets",
+    "Healthy food improves life",
+    "Exercise keeps the body fit",
+    "Solar energy is renewable",
+    "Wind power generates electricity"
+]
+
+#Embeddings
+sen_emb = model.encode(sentences)
+sen_emb = np.array(sen_emb).astype("float32")
+
