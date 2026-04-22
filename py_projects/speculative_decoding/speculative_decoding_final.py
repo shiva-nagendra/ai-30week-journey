0601e29486a3dffafef45ce792bf81d0097107a4
Shiva Nagendra <shivanagendra01@gmail.com>
2026-04-22 16:57:37 +0530

    Speculative Decoding final iteration

--- py_projects/speculative_decoding/speculative_decoding_final.py
+++ py_projects/speculative_decoding/speculative_decoding_final.py
@@ -26,4 +26,68 @@ def normal_decode(prompt, max_length=15):
             next_token_probs = F.softmax(next_token_logits, dim=-1)
             next_token = torch.multinomial(next_token_probs, num_samples=1)
             current_tokens = torch.cat([current_tokens, next_token], dim=1)
+
+            eos_token_id = tokenizer.eos_token_id
+            if next_token.item() == eos_token_id:
+                break
+    return tokenizer.decode(current_tokens[0])
+
+def speculative_decode(prompt, k=5, max_length=15):
+    inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
+    current_tokens = inputs.input_ids
+
+    with torch.no_grad():
+        while current_tokens.shape[1] < max_length:
+            draft_tokens = []
+            draft_probs_list = []
+            temp_tokens = current_tokens.clone()
+
+            for _ in range(k):
+                draft_output = draft_model(input_ids=temp_tokens)
+                draft_logits = draft_output.logits[:, -1, :]
+                draft_probs = F.softmax(draft_logits, dim=-1)
+
+                next_draft_token = torch.multinomial(draft_probs, num_samples=1)
+
+                draft_tokens.append(next_draft_token)
+                draft_probs_list.append(draft_probs[0, next_draft_token.item()].item())
+
+                temp_tokens = torch.cat([temp_tokens, next_draft_token], dim=1)
+
+            draft_tokens_tensor = torch.cat(draft_tokens, dim=1)
+            combined_tokens = torch.cat([current_tokens, draft_tokens_tensor], dim=1)
+            target_output = target_model(input_ids=combined_tokens)
+
+            N = current_tokens.shape[1]
+            all_accepted = True
+            eos_reached = False
+
+            for i in range(k):
+                target_logits_i = target_output.logits[:, N-1+i, :]
+                target_probs_i = F.softmax(target_logits_i, dim=-1)
+
+                guessed_token = draft_tokens[i]
+                p_draft = draft_probs_list[i]
+                p_target = target_probs_i[0, guessed_token.item()].item()
+
+                if p_target < p_draft:
+                    all_accepted = False
+                    break
+
+                if guessed_token.item() == tokenizer.eos_token_id:
+                    eos_reached = True
+                    break
+
+            if all_accepted:
+                current_tokens = torch.cat([current_tokens, draft_tokens_tensor[:, :i+1]], dim=1)
+            else:
+                next_token_logits = target_output.logits[:, N-1, :]
+                next_token_probs = F.softmax(next_token_logits, dim=-1)
+                next_token = torch.multinomial(next_token_probs, num_samples=1)
+                current_tokens = torch.cat([current_tokens, next_token], dim=1)
+
+            if eos_reached:
+                break
+
+    return tokenizer.decode(current_tokens[0])
     
