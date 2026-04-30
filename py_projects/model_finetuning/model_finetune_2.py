44e6a872c721bd1c6c0492e91fe49c498ed145ea
Shiva Nagendra <shivanagendra01@gmail.com>
2026-04-30 18:05:47 +0530

    iteration 2 of model finetuning using QLoRA

---
+++ py_projects/model_finetuning/model_finetune_2.py
@@ -0,0 +1,25 @@
+#iteration 2 of model finetuning using QLoRA
+
+from transformers import autotokenizer, AutoModelForCausalLM, training_arguments
+from peft import LoraConfig, get_peft_model
+from datasets import load_dataset
+from trl import SFTTrainer
+
+#models 
+model_name = "microsoft/phi-2"
+
+tokenizer = autotokenizer.from_pretrained(model_name)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    load_in_4bit=True,
+    device_map="auto"
+)
+
+#Apply LoRA
+
+lora_config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    
+)
