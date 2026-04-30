9fe5d774740e5cb7776a66460a092448f9205b05
Shiva Nagendra <shivanagendra01@gmail.com>
2026-04-30 18:18:22 +0530

    iteration 2 of model fine-tuning using QLoRA

--- py_projects/model_finetuning/model_finetune_2.py
+++ py_projects/model_finetuning/model_finetune_2.py
@@ -1,6 +1,6 @@
 #iteration 2 of model finetuning using QLoRA
 
-from transformers import autotokenizer, AutoModelForCausalLM, training_arguments
+from transformers import autotokenizer, AutoModelForCausalLM, TrainingArguments
 from peft import LoraConfig, get_peft_model
 from datasets import load_dataset
 from trl import SFTTrainer
@@ -17,7 +17,6 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 
 #Apply LoRA
-
 lora_config = LoraConfig(
     r=16,
     lora_alpha=32,
@@ -32,5 +31,14 @@ model = get_peft_model(model, lora_config)
 #load data
 dataset = load_dataset("json", data_files="train.jsonl")
 
+#Trainer setup
+training_args = TrainingArguments(
+    per_device_eval_batch_size=2,
+    gradient_accumulation_steps=4,
+    num_train_epochs=1,
+    logging_steps=10,
+    output_dir="./results",
+    fp16=True
+)
 
 
