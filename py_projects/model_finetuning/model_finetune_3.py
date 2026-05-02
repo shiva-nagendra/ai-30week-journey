825839e88ca0cc8e6460d64d4d296a78b3303b8d
Shiva Nagendra <shivanagendra01@gmail.com>
2026-05-02 14:25:41 +0530

    Microsoft/phi-2model fine-tuning using LoRA third iteration

--- py_projects/model_finetuning/model_finetune_3.py
+++ py_projects/model_finetuning/model_finetune_3.py
@@ -45,7 +45,15 @@ model = AutoModelForCausalLM.from_pretrained(
     config=config # Pass the modified config
 )
 
-
+#Apply LoRA
+lora_config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    target_modules=["q_proj", "v_proj", "k_proj", "o_proj"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
 
 model = get_peft_model(model, lora_config)
 
