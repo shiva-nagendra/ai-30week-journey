4dec8158bd6bf32e985e210e28a2dd68bb676b0e
Shiva Nagendra <shivanagendra01@gmail.com>
2026-05-06 12:40:32 +0530

    Model finetuning testing

--- py_projects/model_finetuning/finetune_save.py
+++ py_projects/model_finetuning/finetune_save.py
@@ -1,2 +1,4 @@
-model.save_pretrained("phi2-business-assistant")
-tokenizer.save_pretrained("phi2-business-assistant")
+#model.save_pretrained("phi2-business-assistant")
+#tokenizer.save_pretrained("phi2-business-assistant")
+
+#coded on google colab and it's commented since not saved in the vscode
