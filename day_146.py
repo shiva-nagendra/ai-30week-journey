319afd5fc4a0288fc3de5db306e3fd7deac9dc62
Shiva Nagendra <shivanagendra01@gmail.com>
2026-05-06 16:51:50 +0530

    Week 24 Day 4: Added caching for faster API responses

--- day_146.py
+++ day_146.py
@@ -62,3 +62,24 @@ def ask_stream(req: QueryRequest):
              media_type="text/plain"
         )
     
+    query_emb = model.encode([query])
+    scores = cosine_similarity(query_emb, doc_emb)[0]
+    top_indices = np.argsort(scores)[::-1][:3]
+
+    context = " ".join([documents[idx] for idx in top_indices])
+
+    prompt = f"""
+You are an AI assistant.
+
+Use ONLY the context below to answer clearly.
+
+Context:
+{context}
+
+Question:
+{query}
+
+Answer:
+"""
+    
+    
