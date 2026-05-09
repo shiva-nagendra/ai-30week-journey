b3881411c8f140165414443ef91c1b1795c8ce05
Shiva Nagendra <shivanagendra01@gmail.com>
2026-05-09 13:51:11 +0530

    Week 24 Project: Production-style async RAG API system

--- py_projects/Async_RAG_API.py
+++ py_projects/Async_RAG_API.py
@@ -16,6 +16,7 @@ from transformers import pipeline
 
 from data import documents
 
+#Load env
 load_dotenv()
 
 APP_MODE = os.getenv("APP_MODE")
@@ -23,12 +24,38 @@ MODEL_NAME = os.getenv("MODEL_NAME")
 
 print(f"Running in: {APP_MODE}")
 
+#FastAPI
 app = FastAPI()
+
+#Load models
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 generator = pipeline(
     "text2text-generation",
     model=MODEL_NAME
 )
+
+#doc embeddings
+doc_emb = model.encode(documents)
+
+#cache
+cache={}
+
+#Request model
+class QueryRequest(BaseModel):
+    query:str
+
+    #streaming
+async def generate_stream(response_text):
+
+    words = response_text.split()
+
+    for word in words:
+        yield word + " "
+        await asyncio.sleep(0.03)
+
+
+
+        
 
 
