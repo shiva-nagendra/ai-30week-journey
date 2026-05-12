31a492137293ae376e5c5ddd47ed65b42f20b708
Shiva Nagendra <shivanagendra01@gmail.com>
2026-05-12 20:05:23 +0530

    Week 25 Day 3: Persistent Chroma vector database for RAG

---
+++ day_151.py
@@ -0,0 +1,10 @@
+#week 25 day 3
+#persistent chroma vector database
+
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+
+import os
+
