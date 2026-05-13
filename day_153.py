04c5c37b8921f2cab3df4758be41207f714510c3
Shiva Nagendra <shivanagendra01@gmail.com>
2026-05-14 00:01:29 +0530

    Week 25 Day 5: Hybrid keyword and semantic retrieval system

---
+++ day_153.py
@@ -0,0 +1,7 @@
+#week 25 day 5
+#Hybrid Search (Keyword + Semantic)
+
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
