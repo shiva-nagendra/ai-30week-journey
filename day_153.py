d3b43839f6718e70c89bb966269f7deb37317f9a
Shiva Nagendra <shivanagendra01@gmail.com>
2026-05-14 00:09:12 +0530

    Week 25 Day 5: Hybrid keyword and semantic retrieval system

--- day_153.py
+++ day_153.py
@@ -37,3 +37,36 @@ semantic_scores = cosine_similarity(
     doc_embeddings
 )[0]
 
+#keyword scores
+query_words = query.lower().split()
+
+keyword_scores = []
+
+for doc in documents:
+
+    doc_lower = doc.lower()
+    score = 0
+
+    for word in query_words:
+        if word in doc_lower:
+            score += 1
+
+    keyword_scores.append(score)
+
+keyword_scores = np.array(keyword_scores)
+
+#normalize keyword scores
+if keyword_scores.max() > 0:
+
+    keyword_scores = keyword_scores / keyword_scores.max()
+
+#hybrid score
+final_scores = (
+    semantic_scores * 0.7
+    +
+    keyword_scores * 0.3
+)
+
+#top scores
+top_indices = np.argsort(final_scores)[::-1][:3]
+
