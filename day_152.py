bfa9ecfa072857b114fc4d36913d16fa394d3e8f
Shiva Nagendra <shivanagendra01@gmail.com>
2026-05-13 22:05:27 +0530

    Week 25 Day 4: Metadata-aware retrieval and filtered vector search

---
+++ day_152.py
@@ -0,0 +1,9 @@
+#week 25 day 4
+#Metadata filtering RAG
+
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.documents import Document
+
+
+
