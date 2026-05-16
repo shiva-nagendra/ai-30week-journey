6d9ff8db719a71d7210a35b513f5065f5dc23106
Shiva Nagendra <shivanagendra01@gmail.com>
2026-05-16 10:52:33 +0530

    Captone RAG FASTAPi project

---
+++ py_projects/Capstone_rag/main.py
@@ -0,0 +1,48 @@
+#week 25 capstone project
+#FASTAPI RAG API
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+from transformers import pipeline
+from langchain_huggingface import HuggingFacePipeline
+
+import os
+
+app = FastAPI()
+
+class QueryRequest(BaseModel):
+    query:str
+
+loader = PyPDFLoader(
+    "py_projects/Capstone_rag/docs/ai_pdf.pdf"
+    )
+
+documents = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=100
+)
+
+chunks = splitter.split_documents(documents)
+
+emb_model = HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2"
+)
+
+db_path = "Capstone_rag/chroma_db"
+
+
+
+
+
+
+
+
