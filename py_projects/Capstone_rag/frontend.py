1cfbff3121dfe16d9ff5acb65ae9c7fadc9025a4
Shiva Nagendra <shivanagendra01@gmail.com>
2026-05-22 15:23:13 +0530

    Week 26 Day 3: Added frontend container and service networking

---
+++ py_projects/Capstone_rag/frontend.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import requests
+
+st.title("RAG AI Assistant")
+
+query = st.text_input("Ask a question")
+
+if st.button("Submit"):
+
+    response = requests.post(
+        "http://backend:8000/predict",
+        json={"query": query}
+    )
+
+    
